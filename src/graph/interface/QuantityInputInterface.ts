f60a1182e78b101e8c23cbe090c849451745b139
Wallbreaker5th <3277946283@qq.com>
2024-02-10 22:31:07 +0800

    Remove ConstantQuantityInput: Its modelValue doesn't work properly, maybe there will be problems?

--- src/graph/interface/QuantityInputInterface.ts
+++ src/graph/interface/QuantityInputInterface.ts
@@ -1,6 +1,6 @@
 import { markRaw } from "vue";
 import { NodeInterface, setType } from "baklavajs";
-import ConstantQuantityInput from "../component/ConstantQuantityInput.vue";
+// import ConstantQuantityInput from "../component/ConstantQuantityInput.vue";
 import { quantityType } from "../InterfaceTypes";
 import { FQuantity } from "../../math/FQuantity";
 
@@ -9,7 +9,7 @@ export class QuantityInputInterface extends NodeInterface<
 > {
   constructor(name: string, value: Array<FQuantity> | FQuantity = new FQuantity(1)) {
     super(name, value);
-    this.setComponent(markRaw(ConstantQuantityInput));
+    // this.setComponent(markRaw(ConstantQuantityInput));
     this.use(setType, quantityType);
   }
 }
