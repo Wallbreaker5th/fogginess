1c65620974678a04cb306e33b11190479d37b67a
Wallbreaker5th <3277946283@qq.com>
2024-02-15 12:29:14 +0800

    Add checkbox for estimateDeltaB in MeasurerInputNode

---
+++ src/graph/component/SmallCheckboxInput.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { NodeInterface } from 'baklavajs';
+import { el } from 'element-plus/es/locale/index.mjs';
+
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true
+    },
+    intf: {
+      type: NodeInterface,
+      required: true
+    }
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    checked: {
+      get() {
+        return this.modelValue;
+      },
+      set(value: boolean) {
+        this.$emit('update:modelValue', value);
+      }
+    },
+    label() {
+      return this.intf.name.split('/')[0];
+    },
+    description() {
+      return this.intf.name.split('/')[1];
+    }
+  },
+  components: { el }
+}
+</script>
+
+<template>
+  <el-checkbox v-model="checked" :label="label" :title="description" style="margin-top: -10px"/>
+</template>
--- src/graph/node/MeasurerInputNode.ts
+++ src/graph/node/MeasurerInputNode.ts
@@ -1,14 +1,11 @@
-import {
-  defineNode,
-  setType,
-  NodeInterface,
-  SelectInterface,
-} from "baklavajs";
+import { defineNode, setType, NodeInterface, SelectInterface } from "baklavajs";
+import { markRaw } from "vue";
 import { UnitInterface } from "../interface/UnitInterface";
 import { NumberInterface } from "../interface/NumberInterface";
 import { DistributionType, FMeasurer } from "../../math/FMeasurer";
 import { measurerType } from "../InterfaceTypes";
 import { FQuantity } from "../../math/FQuantity";
+import SmallCheckboxInput from "../component/SmallCheckboxInput.vue"
 
 export default defineNode({
   type: "MeasurerInputNode",
@@ -17,6 +14,13 @@ export default defineNode({
     unit: () => new UnitInterface("单位", new FQuantity(1)).setPort(true),
     MPE: () => new NumberInterface("最大允差", 0).setPort(false),
     EE: () => new NumberInterface("估计误差", 0).setPort(false),
+    estimateDeltaB: () =>
+      new NodeInterface<boolean>(
+        "忽略较小一项/若最大允差和估计误差的较大者超过较小者的三倍，则忽略较小的一项",
+        true
+      )
+        .setComponent(markRaw(SmallCheckboxInput))
+        .setPort(false),
     distributionType: () =>
       new SelectInterface("分布类型", DistributionType.Normal, [
         { text: "正态分布", value: DistributionType.Normal },
