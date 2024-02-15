ff10d2178353ce74792156d46c3402598d8f0fe3
Wallbreaker5th <3277946283@qq.com>
2024-02-15 12:13:29 +0800

    Paste from excel for ConstantArrayInput

--- src/graph/component/ConstantArrayInput.vue
+++ src/graph/component/ConstantArrayInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Delete, Plus } from '@element-plus/icons-vue'
+import { Delete, Plus, Check, Close } from '@element-plus/icons-vue'
 </script>
 <script lang="ts">
 export default {
@@ -10,6 +10,12 @@ export default {
     }
   },
   emits: ['update:modelValue'],
+  data() {
+    return {
+      visible: false,
+      textInput: ''
+    }
+  },
   computed: {
     array: {
       get() {
@@ -47,6 +53,19 @@ export default {
           (input as HTMLInputElement).focus();
         }
       });
+    },
+    fromText(text: string) {
+      // split by blank characters
+      const parts = text.split(/\s+/);
+      const result = [];
+      for (let part of parts) {
+        if (part === '') {
+          continue;
+        }
+        result.push(parseFloat(part));
+      }
+      this.array = result;
+      this.visible = false;
     }
   }
 }
@@ -57,13 +76,25 @@ export default {
   <div class="constant-array-input-table">
     <div v-for="index in modelValue.length" :key="index" class="constant-array-input-cell">
       <el-input-number :modelValue="modelValue[index - 1]" @update:modelValue="updateItem(index - 1, $event)" size="small"
-        class="constant-array-input-box" :controls="false" title="" :ref="'input' + index" @keydown.enter="moveFocus(index+1)"
-        @keydown.tab="moveFocus(index+1)" />
+        class="constant-array-input-box" :controls="false" title="" :ref="'input' + index"
+        @keydown.enter="moveFocus(index + 1)" @keydown.tab="moveFocus(index + 1)" />
       <el-button type="danger" :icon="Delete" size="small" class="constant-array-input-delete"
         @click="removeItem(index - 1)" circle />
     </div>
-    <div class="constant-array-input-cell">
+    <div class="constant-array-input-tools">
       <el-button type="primary" :icon="Plus" size="small" class="constant-array-input-add" @click="addItem" />
+      <el-popover :visible="visible" placement="top" :width="200">
+        <div class="constant-array-input-pop">
+          <el-input type="textarea" v-model="textInput" placeholder="从 Excel 粘贴，或直接输入一组用空格分隔的数值" size="small" />
+          <div class="constant-array-input-tools">
+            <el-button type="primary" size="small" @click="fromText(textInput)" :icon="Check" />
+            <el-button size="small" @click="visible = false" :icon="Close" />
+          </div>  
+        </div>
+        <template #reference>
+          <el-button @click="visible = true" size="small">从 Excel 粘贴</el-button>
+        </template>
+      </el-popover>
     </div>
   </div>
 </template>
@@ -84,6 +115,14 @@ export default {
   margin: 3px;
 }
 
+.constant-array-input-tools {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 3px;
+}
+
 .constant-array-input-box {
   margin: 0 5px;
 }
@@ -94,5 +133,17 @@ export default {
 
 .constant-array-input-add {
   width: 50px;
+}
+
+.constant-array-input-pop {
+  font-size: 0.8em;
+}
+
+.constant-array-input-pop .constant-array-input-tools {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 3px;
 }
 </style>
